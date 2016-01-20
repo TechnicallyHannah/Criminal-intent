1d7b822cc59c445a312548fef9b55b1b14b4c2d2
Hpaulson <technicalhannah@gmail.com>
2016-01-20 10:49:51 -0700

    runs correctly infltating recycler view

--- app/src/main/java/com/example/a712948/criminal_intent/CrimeListFragment.java
+++ app/src/main/java/com/example/a712948/criminal_intent/CrimeListFragment.java
@@ -1,11 +1,27 @@
 package com.example.a712948.criminal_intent;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * @author Hannah Paulson
  * @since 1/4/16.
  */
 public class CrimeListFragment extends Fragment {
+    private RecyclerView mRecyclerView;
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
+
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return view;
+    }
 }
