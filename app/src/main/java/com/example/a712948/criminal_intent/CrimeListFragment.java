e4d1b0fa309a9923fdd7b45299e3f6a980b3ff72
Hpaulson <technicalhannah@gmail.com>
2016-01-20 11:02:08 -0700

    App uses recycler view and adds numbers

--- app/src/main/java/com/example/a712948/criminal_intent/CrimeListFragment.java
+++ app/src/main/java/com/example/a712948/criminal_intent/CrimeListFragment.java
@@ -7,6 +7,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
 
 /**
  * @author Hannah Paulson
@@ -14,6 +17,7 @@ import android.view.ViewGroup;
  */
 public class CrimeListFragment extends Fragment {
     private RecyclerView mRecyclerView;
+    private CrimeAdapter mAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -22,6 +26,52 @@ public class CrimeListFragment extends Fragment {
         mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
+        updateUI();
         return view;
     }
+
+    private class CrimeHolder extends RecyclerView.ViewHolder {
+        public TextView mTextView;
+
+        public CrimeHolder(View itemView) {
+            super(itemView);
+
+            mTextView = (TextView) itemView;
+        }
+    }
+
+    private void updateUI() {
+        CrimeLab crimeLab = CrimeLab.getCrimeLab(getActivity());
+        List<Crime> crimes = crimeLab.getCrimes();
+
+        mAdapter = new CrimeAdapter(crimes);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder> {
+        private List<Crime> mCrimes;
+
+        public CrimeAdapter(List<Crime> crimes) {
+            mCrimes = crimes;
+        }
+
+        @Override
+        public CrimeHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            View view = layoutInflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+            return new CrimeHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(CrimeHolder holder, int position) {
+            Crime crime = mCrimes.get(position);
+            holder.mTextView.setText(crime.getTitle());
+        }
+
+        @Override
+        public int getItemCount() {
+            return mCrimes.size();
+        }
+    }
+
 }
