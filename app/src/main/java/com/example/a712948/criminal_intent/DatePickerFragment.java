5005a4acf408bb0b2c66cb1a874a7cb4f13074ec
Hpaulson <technicalhannah@gmail.com>
2016-01-25 11:31:42 -0700

    dialog box shows a date picker

--- app/src/main/java/com/example/a712948/criminal_intent/DatePickerFragment.java
+++ app/src/main/java/com/example/a712948/criminal_intent/DatePickerFragment.java
@@ -3,6 +3,8 @@ package com.example.a712948.criminal_intent;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
 
 /**
  * @author Hannah Paulson
@@ -10,7 +12,10 @@ import android.support.v7.app.AlertDialog;
  */
 public class DatePickerFragment extends android.support.v4.app.DialogFragment {
     public Dialog onCreateDialog(Bundle bundle) {
+        View v = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_date, null);
+
         return new AlertDialog.Builder(getActivity())
+                .setView(v)
                 .setTitle(R.string.date_picker_title)
                 .setPositiveButton(android.R.string.ok, null)
                 .create();
