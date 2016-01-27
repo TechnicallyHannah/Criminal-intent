ee7310f27e5395a76e072558cc53935471db938f
Hpaulson <technicalhannah@gmail.com>
2016-01-27 11:54:53 -0700

    List view add's menu at the top

--- app/src/main/java/com/example/a712948/criminal_intent/CrimePagerActivity.java
+++ app/src/main/java/com/example/a712948/criminal_intent/CrimePagerActivity.java
@@ -4,9 +4,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 import java.util.List;
 import java.util.UUID;
@@ -15,7 +15,7 @@ import java.util.UUID;
  * @author Hannah Paulson
  * @since 1/25/16.
  */
-public class CrimePagerActivity extends FragmentActivity {
+public class CrimePagerActivity extends AppCompatActivity {
     private static final String ARG_CRIME_ID = "crime_id";
     private ViewPager mViewPager;
     private List<Crime> mCrimes;
--- app/src/main/java/com/example/a712948/criminal_intent/SingleFragmentActivity.java
+++ app/src/main/java/com/example/a712948/criminal_intent/SingleFragmentActivity.java
@@ -2,20 +2,20 @@ package com.example.a712948.criminal_intent;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * @author Hannah Paulson
  * @since 12/29/15.
  */
-public abstract class SingleFragmentActivity extends FragmentActivity {
+public abstract class SingleFragmentActivity extends AppCompatActivity {
     protected abstract Fragment createFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
+        setContentView(R.layout.activity_crime);
 
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.container);
